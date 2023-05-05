4347ec07fe849c1dbeca64a5221ee3495ee23518
Yijing Huang <hyj891204@gmail.com>
2023-05-05 18:20:07 -0500

    feat: remove editor style from link menu - add utils function and widgets for overlay - organize utils folder - add constant text into AppFlowyEditorLocalizations - improve pop up menu style

--- lib/src/editor/toolbar/items/color/color.dart
+++ lib/src/editor/toolbar/items/color/color.dart
@@ -1,4 +1,4 @@
+export 'color_menu.dart';
+export 'color_picker.dart';
 export 'highlight_color_toolbar_item.dart';
 export 'text_color_toolbar_item.dart';
-export 'color_picker.dart';
-export 'color_menu.dart';
