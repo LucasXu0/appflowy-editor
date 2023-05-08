a105b963de6eb44ed21e2e29cbf6abfdad0f5ea7
Yijing Huang <hyj891204@gmail.com>
2023-05-08 17:26:37 -0500

    fix: fix tooltip text

--- lib/src/editor/toolbar/items/link/link_toolbar_item.dart
+++ lib/src/editor/toolbar/items/link/link_toolbar_item.dart
@@ -21,7 +21,7 @@ final linkItem = ToolbarItem(
     return IconItemWidget(
       iconName: 'toolbar/link',
       isHighlight: isHref,
-      tooltip: '${AppFlowyEditorLocalizations.current.link}}',
+      tooltip: AppFlowyEditorLocalizations.current.link,
       onPressed: () {
         showLinkMenu(context, editorState, selection, isHref);
       },
